50ea89e44564cd873b4b2f4acbfe35e851674000
Ricardo Rius <ricardorius@live.com.mx>
2018-11-16 11:41:03 -0600

    Update configs

--- truffle.js
+++ truffle.js
@@ -1,18 +1,95 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+//HD Wallet for keyless servers (infura)
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const ganacheRPC = require("ganache-cli");
+
+let provider
+
+function getNmemonic() {
+  try{
+    return require('fs').readFileSync("./seed", "utf8").trim();
+  } catch(err){
+    return "";
+  }
+}
+
+function getProvider(rpcUrl) {
+  if (!provider) {
+    provider = new HDWalletProvider(getNmemonic(), rpcUrl)
+  }
+  return provider
+}
+
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  solc: {
+    optimizer: {
+      enabled: false,
+      runs: 200
+    }
+  },
+  networks: {
+    ganache: {
+      get provider() {
+        if (!provider) {
+          provider = ganacheRPC.provider({total_accounts: 10, network_id: 35, gasLimit:8000000, gasPrice: 1000000000})
+        }
+        return provider
+      },
+      network_id: 35,
+      gas: 8000000,
+      gasPrice: 1000000000
+    },
+    geth_dev: { 
+      host: "localhost",
+      network_id: 1337,
+      port: 8545,
+      gas: 6283185, //geth --dev gas limit is 6283185
+      gasPrice: 1000000000
+    },
+    ropsten: {
+      get provider() {
+        return getProvider("https://ropsten.infura.io/")
+      },
+      gas: 4004580,
+      network_id: 3
+    },
+    rinkeby: {
+      get provider() {
+        return getProvider("https://rinkeby.infura.io/")
+      },
+      network_id: 4
+    },
+    mainnet: {
+      get provider() {
+        return getProvider("https://mainnet.infura.io/")
+      },
+      gas: 1704580,
+      gasPrice: 1000000000,
+      network_id: 1
+    },
+    sokol: {
+      get provider() {
+        return getProvider("https://sokol.poa.network/")
+      },
+      gas: 8000000,
+      gasPrice: 1000000000,
+      network_id: "*"
+    },
+    poa: {
+      get provider() {
+        return getProvider("https://core.poa.network")
+      },
+      gas: 1704580,
+      gasPrice: 1000000000,
+      network_id: "*"
+    },
+    dai: {
+      get provider() {
+        return getProvider("https://dai.poa.network")
+      },
+      gas: 1704580,
+      gasPrice: 1000000000,
+      network_id: "*"
+    }
+  }
 };
